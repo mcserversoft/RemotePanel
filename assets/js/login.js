033dab1b34909b793a4d3c8082ca77e055ac92a1
Fiahblade <robin.gabriel@pandora.be>
2022-02-14 13:33:53 +0100

    Some login code cleanup & removed unneeded logging

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,40 +1,81 @@
+document.addEventListener("DOMContentLoaded", function () {
+    checkConnection();
+});
+
+function checkConnection() {
+    axios.get(`/`).then(() => {
+        $("#login-card2").replaceWith(`
+		<div class="" id="cred-status">
+        </div>
+        <form action="#" onsubmit="login(); return false;">
+        <div class="uk-margin uk-width-expand">
+            <div class="uk-inline">
+                <span class="uk-form-icon" uk-icon="icon: user"></span>
+                <input class="uk-input" id="username" type="text" placeholder="Username">
+            </div>
+        </div>                
+        <div class="uk-margin uk-width-expand">
+            <div class="uk-inline">
+                <span class="uk-form-icon uk-form-icon" uk-icon="icon: lock"></span>
+                <input class="uk-input" id="password" type="password" placeholder="Password">
+                <input class="uk-hidden" type="submit">
+            </div>
+        </div>
+        <a type="submit" onclick="login()" class="uk-button uk-button-default uk-width-5-6 uk-width-3-4@s uk-width-3-4@m uk-width-1-1@l">Login</a>    
+    </form>
+        `);
+    }
+    ).catch(err => {
+        $("#login-card").replaceWith(`
+        <div class="uk-card uk-card-secondary uk-card-body uk-padding-large uk-animation-shake">
+        <div class="uk-overlay uk-margin-remove-bottom uk-padding-remove-bottom uk-position-top-right uk-visible@s">
+            <a href="" class="uk-icon-button" uk-toggle="target: #modal-api-url" uk-icon="cog"></a>
+        </div>
+        <h1 class="uk-card-title uk-text-bold uk-margin-remove-top uk-padding-remove-top">MCSS Login</h1>
+        <img class="uk-margin-medium-bottom" src="/assets/img/logo.png" alt="" width="100" height="100">
+        <div class="">
+            <span uk-icon="icon: warning; ratio: 2"></span>
+            <p>Unable to reach the API</p>
+            <a class="uk-text-success" href="#" uk-toggle="target: #modal-close-default2">Why?</a>
+        </div>
+        <a class="uk-icon-button uk-margin-medium-top uk-hidden@s" uk-toggle="target: #modal-api-url" uk-icon="cog"></a>  
+    </div>
+        `);
+    });
+}
+
 function login() {
     let token = localStorage.getItem('token')
 
-    if (token != null) {
-        console.log("Token found")
-    }
-    else {
-        console.log("No token found locally")
+    if (token == null) {
         let username = document.getElementById("username").value;
         let password = document.getElementById("password").value;
-        const request = new Request(`/api/token`, {method: 'POST', body: new URLSearchParams({'username': username, 'password': password})})
+        const request = new Request(`/api/token`, { method: 'POST', body: new URLSearchParams({ 'username': username, 'password': password }) })
+
         fetch(request).then(function (response) {
             try {
                 if (response.status === 200) {
                     response.json().then((result) => {
                         JSON.stringify(result)
                         let token = result["access_token"]
-                        console.log(token);
-                        localStorage.setItem("token",token)
+
+                        localStorage.setItem("token", token)
                         window.location.href = "/panel.html";
                     })
                 } else if (response.status === 401) {
-                    console.log(response.status)
-                    console.log("bad credentials")
                     $("#cred-status").replaceWith(`<div class="" id="cred-status">
-                    <p style="color: rgb(255, 0, 0);">Wrong Password</p>
+                    <p style="color: #cf3755;">Your username and/or password do not match.</p>
                     </div>`)
                 } else if (response.status === 500) {
-                    console.log("we done broke something")
+                    console.log(response.status)
                     $("#cred-status").replaceWith(`<div class="" id="cred-status">
-                    <p style="color: rgb(255, 0, 0);">Something Went Wrong</p>
+                    <p style="color: #cf3755;">Something went wrong.</p>
                     </div>`)
                 } else {
-                    console.log("API could not be reached, error code is " + response.status)
+                    console.log("API could not be reached, error code: " + response.status)
                 }
             } catch (error) {
-                console.log("error")
+                console.log("Failed to login: " + error)
             }
         })
     }
--- assets/js/test4.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let token = localStorage.getItem('token')
-        if (token === null) {
-            console.log("Token Not found")
-            window.location.href = "/index.html";
-        } else {
-            console.log("Token Found")
-        }
-});
