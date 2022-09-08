bdc589b0f95a7ee02b2f7cb4774fe3ba53cc7f99
Fiahblade <robin.gabriel@pandora.be>
2022-09-08 19:40:09 +0200

    Added support for versioning

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 	// for more information about preprocessors
 	preprocess: preprocess({
 		postcss: true,
-	  }),
+	}),
 
 	kit: {
 		adapter: adapter({
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	define: {
+		__VERSION__: JSON.stringify(process.env.npm_package_version)
+	}
 };
 
 export default config;
