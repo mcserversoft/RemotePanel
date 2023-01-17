4aba60865aed0c72db8320557cc1096f7de788af
Fiahblade <robin.gabriel@pandora.be>
2023-01-17 18:07:19 +0100

    Add flowbite

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: { "50": "#d5e999", "100": "#c1de66", "200": "#c1de66", "300": "#b6d94d", "400": "#acd333", "500": "#a1ce1a", "600": "#97c800", "700": "#88b400", "800": "#6a8c00", "900": "#4c6400" }
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
+  darkMode: 'class',
 }
