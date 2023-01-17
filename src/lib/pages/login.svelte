3ae33379d55b888c906556a6e2f5cd974736fb34
Fiahblade <robin.gabriel@pandora.be>
2023-01-17 18:10:33 +0100

    Refreshed login page

--- src/lib/pages/login.svelte
+++ src/lib/pages/login.svelte
@@ -2,6 +2,11 @@
 	import { login, LoginFailureReason } from '$lib/code/auth';
 	import FormError from '$lib/components/formError.svelte';
 	import Logo from '$lib/svgs/Logo.svelte';
+	import { PanelTheme } from '../../types';
+	import { AccordionItem, Accordion, Button, Label, Input, Tooltip } from 'flowbite-svelte';
+	import { mdiAccount, mdiLockAlert, mdiWeatherSunny } from '@mdi/js';
+	import Icon from '$lib/components/icon.svelte';
+	import ThemeToggler from '$lib/components/themeToggler.svelte';
 
 	interface Error {
 		visible: boolean;
@@ -46,41 +51,63 @@
 	<title>MCSS Remote Panel | Login</title>
 </svelte:head>
 
-<section class="h-screen bg-login-background bg-cover bg-blend-color-dodge bg-zinc-800">
-	<div class="container mx-auto max-w-md h-full">
-		<div class="flex flex-col place-content-center h-full text-white">
-			<div class="p-10 rounded-xl sm:bg-mcss-gray">
-				<Logo className="mx-auto mb-8" width="96" height="96" />
-				<p class="mb-4 text-xl text-center font-semibold">Remote Login</p>
+<section class="bg-gray-50 dark:bg-gray-900 min-h-screen">
+	<div class="flex justify-end p-5">
+		<ThemeToggler />
+	</div>
 
-				<form on:submit|preventDefault={onLogin}>
-					<div class="form-control mb-3">
-						<label class="label" for="username">
-							<span class="label-text">Username</span>
-						</label>
-						<input bind:value={username} required id="username" name="username" type="text" placeholder="" class="input input-bordered " />
+	<div class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
+		<Logo className="mx-auto mb-4" width="96" height="96" />
+		<p class="mb-8 text-center text-2xl font-semibold dark:text-white">MC Server Soft Remote</p>
+
+		<div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+			<div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+				<h1 class="text-center text-xl font-bold leading-tight tracking-tight md:text-2xl dark:text-white">Log in to your account</h1>
+				<form on:submit|preventDefault={onLogin} class="space-y-4 md:space-y-6">
+					<div>
+						<Label for="username" class="mb-2">Username</Label>
+						<Input bind:value={username} type="text" id="username" placeholder="John" required />
 					</div>
 
-					<div class="form-control mb-6">
-						<label class="label" for="password">
-							<span class="label-text">Password</span>
-						</label>
-						<input bind:value={password} required id="password" name="password" type="password" placeholder="" class="input input-bordered " />
+					<div>
+						<Label for="password" class="mb-2">Password</Label>
+						<Input bind:value={password} type="password" id="password" placeholder="•••••••••" required />
 					</div>
 
-					<button type="submit" class="btn btn-primary btn-block mb-3">Login</button>
+					<Button type="submit" class="w-full" color="primary">Log in</Button>
+
 					{#if error.visible}
 						<FormError title={error.title} message={error.message} />
 					{/if}
 
-					<div class="collapse text-center">
-						<input type="checkbox" />
-						<div class="collapse-title text-gray-400 px-0">How do I create an account?</div>
-						<div class="collapse-content text-gray-300">
-							<p>Accounts can be created/edited in the Web Panel section of MC Server Soft.</p>
-							<a href="https://docs.mcserversoft.com/webpanel/users" target="_blank" rel="noopener noreferrer" class="link link-primary">View documentation</a>
-						</div>
-					</div>
+					<p class="text-center text-lg font-medium text-gray-500 dark:text-gray-400">FAQ</p>
+					<Accordion>
+						<AccordionItem>
+							<span slot="header" class="text-base flex gap-2">
+								<Icon data={mdiAccount} />
+								<span>How do I create an account?</span>
+							</span>
+
+							<p>Accounts can be created and edited in the Web Panel section of MC Server Soft.</p>
+							<a href="https://docs.mcserversoft.com/webpanel/users" target="_blank" rel="noopener noreferrer" class="text-primary-600 dark:text-primary-500 hover:underline">View documentation</a>
+						</AccordionItem>
+						<AccordionItem>
+							<span slot="header" class="text-base flex gap-2">
+								<Icon data={mdiLockAlert} />
+								<span>What is this SSL warning? Is this safe?</span>
+							</span>
+
+							<p class="mb-2 dark:text-gray-400">Yes it's safe, this self-signed certificates provides a secure connection to the panel even though it is not issued by a trusted third-party certificate authority.</p>
+							<p class="mb-2 dark:text-gray-400">While browsers and other software may not recognize it as trusted by default, it still encrypt and decrypt data transmitted between the website and you.</p>
+
+							<p class="mb-2 dark:text-gray-400">To remove the warning, you can:</p>
+							<ul class="list-disc list-inside">
+								<li>Trust the certificate</li>
+								<li>Setup a reverse proxy</li>
+								<li>Use <a href="https://mcss.app" target="_blank" rel="noopener noreferrer" class="text-primary-600 dark:text-primary-500  hover:underline">mcss.app</a></li>
+							</ul>
+						</AccordionItem>
+					</Accordion>
 				</form>
 			</div>
 		</div>
