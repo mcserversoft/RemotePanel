ccc5fe32da85b5131a041b77aef6d0b29110c8fe
Fiahblade <robin.gabriel@pandora.be>
2023-01-26 13:02:11 +0100

    Minor tweaks

--- src/lib/layouts/header.svelte
+++ src/lib/layouts/header.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { auth, logout } from '$lib/code/auth';
 	import { Page, navigateToPage } from '$lib/code/routing';
-	import Dashboard from '$lib/pages/dashboard.svelte';
-	import Logo from '$lib/svgs/Logo.svelte';
-	import { Navbar, NavBrand, Avatar, Dropdown, DropdownItem, DropdownHeader, DropdownDivider } from 'flowbite-svelte';
+	import { Navbar, Dropdown, DropdownItem, DropdownDivider } from 'flowbite-svelte';
+	import { mdiMenuDown } from '@mdi/js';
+	import Icon from '$lib/components/icon.svelte';
 
 	let dropdownOpen: boolean = false;
 
@@ -15,13 +15,13 @@
 
 <Navbar navClass=" px-2 sm:px-4 py-2.5 absolute w-full top-0 left-0 border-b dark:bg-gray-800">
 	<div />
-	<div class="flex items-center md:order-2">
-		<Avatar id="avatar-menu" src="https://placeimg.com/80/80/people" />
+	<div class="flex items-center md:order-2 min-h-[45px]">
+		<button class="flex font-semibold cursor-pointer border-transparent focus:border-transparent focus:ring-0" id="dropdown-toggle">
+			{$auth.username}
+			<Icon data={mdiMenuDown} />
+		</button>
 	</div>
-	<Dropdown bind:open={dropdownOpen} placement="bottom" triggeredBy="#avatar-menu">
-		<DropdownHeader>
-			<span class="block truncate text-sm font-medium">{$auth.username} </span>
-		</DropdownHeader>
+	<Dropdown bind:open={dropdownOpen} placement="bottom" triggeredBy="#dropdown-toggle">
 		<DropdownItem on:click={() => onNavigateClick(Page.Settings)}>Settings</DropdownItem>
 		<DropdownItem on:click={() => onNavigateClick(Page.About)}>About</DropdownItem>
 		<DropdownDivider />
--- src/lib/layouts/sidebar.svelte
+++ src/lib/layouts/sidebar.svelte
@@ -17,7 +17,6 @@
 
 	function checkIfAllowedToResize() {
 		if (canCollapse) {
-			console.log('gogo');
 			collapseSidebar = get(settings).collapseSidebar;
 		}
 	}
@@ -44,16 +43,7 @@
 					<span class="ml-3 {collapseSidebar ? 'hidden ' : ''}">Dashboard</span>
 				</a>
 			</li>
-			<li>
-				<a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-					<svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
-						><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" /></svg
-					>
-					<span class="{collapseSidebar ? 'hidden' : ''} flex-1 ml-3 whitespace-nowrap">Kanban</span>
-					<span class="{collapseSidebar ? 'hidden' : ''} inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
-				</a>
-			</li>
-			<li>
+			<!-- <li>
 				<a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
 					<svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
 						><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z" /><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z" /></svg
@@ -61,8 +51,17 @@
 					<span class="{collapseSidebar ? 'hidden' : ''} flex-1 ml-3 whitespace-nowrap">Servers</span>
 					<span class="{collapseSidebar ? 'hidden' : ''} inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-600 bg-blue-200 rounded-full dark:bg-blue-900 dark:text-blue-200">3</span>
 				</a>
-			</li>
-			<li>
+			</li> -->
+			<!-- <li>
+				<a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+					<svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
+						><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" /></svg
+					>
+					<span class="{collapseSidebar ? 'hidden' : ''} flex-1 ml-3 whitespace-nowrap">Kanban</span>
+					<span class="{collapseSidebar ? 'hidden' : ''} inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
+				</a>
+			</li> -->
+			<!-- <li>
 				<a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
 					<svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
 						><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd" /></svg
@@ -77,7 +76,7 @@
 					>
 					<span class="{collapseSidebar ? 'hidden' : ''} flex-1 ml-3 whitespace-nowrap">Sign In</span>
 				</a>
-			</li>
+			</li> -->
 			<li>
 				<a href="/" on:click={() => navigateToPage(Page.Settings)} class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
 					<Icon data={mdiCog} class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
