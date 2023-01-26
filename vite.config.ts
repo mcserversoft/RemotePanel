3870d85e1b4e620966c41eadc270340f4d717220
Fiahblade <robin.gabriel@pandora.be>
2023-01-26 16:12:27 +0100

    Auto zip on Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import zipPack from 'vite-plugin-zip-pack';
+
+import { sveltekit } from '@sveltejs/kit/vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), zipPack({ inDir: 'build', outDir: 'build', outFileName: 'panel.zip' })],
 	define: {
 		__VERSION__: JSON.stringify(process.env.npm_package_version)
 	}
