30df5c72d407a6dc5accc9f53230cfc67651babf
Fiahblade <robin.gabriel@pandora.be>
2023-02-08 15:16:32 +0100

    Added vite-pwa/sveltekit

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import type { UserConfig } from 'vite';
 import zipPack from 'vite-plugin-zip-pack';
 
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 
 const config: UserConfig = {
-	plugins: [sveltekit(), zipPack({ inDir: 'build', outDir: 'build', outFileName: 'panel.zip' })],
+	plugins: [sveltekit(), SvelteKitPWA(), zipPack({ inDir: 'build', outDir: 'build', outFileName: 'panel.zip' })],
 	define: {
 		__VERSION__: JSON.stringify(process.env.npm_package_version)
 	}
