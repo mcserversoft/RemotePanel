3a8adf3354c0f62c13546f1e3488446de90604d6
Fiahblade <robin.gabriel@pandora.be>
2023-06-21 20:11:27 +0200

    Added serverPermSelector select logic

--- src/lib/components/server/serverPermSelector.svelte
+++ src/lib/components/server/serverPermSelector.svelte
@@ -8,10 +8,12 @@
 	export let selection: any = [];
 	let savedSelection: any = [];
 
+	let isCustomServerSelected: boolean = false;
 	let showCustomServersModal: boolean = false;
 
 	function handleSave() {
 		showCustomServersModal = false;
+		isCustomServerSelected = true;
 		savedSelection = selection;
 	}
 
@@ -31,12 +33,27 @@
 
 <div class="flex space-x-4">
 	<div class="flex items-center">
-		<input checked id="default-radio-2" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-		<label for="default-radio-2" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">All</label>
+		<input
+			checked
+			id="radio-selection-all"
+			name="radio-selection"
+			value={false}
+			bind:group={isCustomServerSelected}
+			type="radio"
+			class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+		/>
+		<label for="radio-selection-all" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">All</label>
 	</div>
 	<div class="flex items-center">
-		<input id="default-radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-		<label for="default-radio-1" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Custom ({selection.length})</label>
+		<input
+			id="radio-selection-custom"
+			name="radio-selection"
+			value={true}
+			bind:group={isCustomServerSelected}
+			type="radio"
+			class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+		/>
+		<label for="radio-selection-custom" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Custom ({selection.length})</label>
 	</div>
 	<div class="flex items-center">
 		<form on:submit|preventDefault={handleModalCustomServersToggle}>
