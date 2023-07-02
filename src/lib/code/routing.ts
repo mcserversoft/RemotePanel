8fecdea9bcb24db64811bf37a233170de32fb491
Fiahblade <robin.gabriel@pandora.be>
2023-07-02 23:41:50 +0200

    Update routing.ts

--- src/lib/code/routing.ts
+++ src/lib/code/routing.ts
@@ -7,7 +7,6 @@ export const baseUrl = '';
 export const selectedPage = persisted('selectedPage', Page.Dashboard);
 export const selectedPageProps = persisted('selectedPageProps', null);
 
-export function navigateToPage(pageName: Page) {
 export function navigateToPage(pageName: Page, props: any = null) {
     selectedPage.set(pageName);
 
