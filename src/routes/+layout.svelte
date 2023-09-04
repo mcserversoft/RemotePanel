52ec409a1e0db7b2a00d594e7db9d744b5de5cdc
Fiahblade <robin.gabriel@pandora.be>
2023-09-04 20:53:07 +0200

    Fix PWA

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script>
 	import ThemeWatcher from '$lib/components/core/themeWatcher.svelte';
+	import { pwaInfo } from 'virtual:pwa-info';
 	import '../app.css';
+
+	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 </script>
+
+<svelte:head>
+	{@html webManifestLink}
+</svelte:head>
 
 <slot />
 <ThemeWatcher />
