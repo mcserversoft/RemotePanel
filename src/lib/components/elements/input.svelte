45f511ec1e023bf24b2a33ddf1d34f30850361a9
Fiahblade <robin.gabriel@pandora.be>
2023-11-11 13:53:09 +0100

    Added support for input readonly

--- src/lib/components/elements/input.svelte
+++ src/lib/components/elements/input.svelte
@@ -8,7 +8,8 @@
 	export let type: HTMLInputTypeAttribute = 'text';
 	export let min: string = '';
 	export let max: string = '';
-	export let required: boolean;
+	export let required: boolean = false;
+	export let readonly: boolean = false;
 	export let error: boolean = false;
 	export let inputClass: string = '';
 
@@ -17,22 +18,24 @@
 	function handleInputChange() {
 		dispatch('input', value);
 	}
+
+	function getStyling() {
+		let style: string = 'bg-gray-50 text-gray-900  border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:border-blue-500 focus:ring-blue-500 dark:focus:border-blue-500 dark:focus:ring-blue-500';
+
+		if (error) {
+			style = 'bg-red-50 text-red-900 border-red-500 dark:bg-gray-700 placeholder-red-700 dark:placeholder-red-500 dark:border-red-500 dark:text-red-500 focus:border-red-500 focus:ring-red-500 dark:focus:border-red-500 dark:focus:ring-red-500';
+		}
+
+		if (readonly) {
+			style += '!text-gray-500 dark:!text-gray-400 pointer-events-none';
+		}
+
+		return `${style} ${inputClass}`;
+	}
 </script>
 
 <label for={label} class="block mb-2 text-sm font-medium">{label}</label>
-<input
-	{...{ type }}
-	bind:value
-	on:input={handleInputChange}
-	id={label}
-	class="block w-full p-2.5 text-sm rounded-lg {error
-		? 'bg-red-50 text-red-900 border-red-500 dark:bg-gray-700 placeholder-red-700 dark:placeholder-red-500 dark:border-red-500 dark:text-red-500 focus:border-red-500 focus:ring-red-500  dark:focus:border-red-500 dark:focus:ring-red-500'
-		: 'bg-gray-50 text-gray-900  border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:border-blue-500 focus:ring-blue-500 dark:focus:border-blue-500 dark:focus:ring-blue-500 '} {inputClass}"
-	{placeholder}
-	{required}
-	{min}
-	{max}
-/>
+<input {...{ type }} bind:value on:input={handleInputChange} id={label} class="block w-full p-2.5 text-sm rounded-lg {getStyling()} " {placeholder} {required} {min} {max} {readonly} />
 
 <p class="mt-3 text-sm text-gray-500 dark:text-gray-400">
 	<slot />
