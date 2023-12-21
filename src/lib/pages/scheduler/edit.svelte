7a6be474272553b61b8eac7195514b5fc8627521
Fiahblade <robin.gabriel@pandora.be>
2023-12-21 17:59:10 +0100

    Fix scheduler name edit

--- src/lib/pages/scheduler/edit.svelte
+++ src/lib/pages/scheduler/edit.svelte
@@ -146,7 +146,7 @@
 
 		<form on:submit|preventDefault={updateTask}>
 			<BoxedContainer class="space-y-3">
-				<Input bind:value={name} label={'Name'} type={'string'} placeholder={'Task name'} required={true} />
+				<Input bind:value={name} label={'Name'} type={'string'} placeholder={'Task name'} required={true} on:input={handleInputChange} />
 			</BoxedContainer>
 			<BoxedContainer class="space-y-3">
 				<span class="space-y-1 text-sm font-medium">
